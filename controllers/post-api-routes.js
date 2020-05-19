21d30d6a55db5dcac8fca0739886b8a692c029f6
Aliagho <ali.bahrami633@yahoo.com>
2020-05-19 22:09:18 +1000

    handling some routes - error handling added

--- controllers/post-api-routes.js
+++ controllers/post-api-routes.js
@@ -2,23 +2,26 @@ var db = require("../models");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
-  // GET route for getting all of the posts
   app.get("/api/posts", function (req, res) {
-    db.Post.findAll({}).then(function (dbPost) {
+    db.Post.findAll({
+      order: [["updatedAt", "DESC"]],
+      include: [db.User],
+    }).then(function (dbPost) {
       res.json(dbPost);
     });
   });
 
-  // POST route for saving a new post
   app.post("/api/posts", isAuthenticated, function (req, res) {
     console.log(req.body);
     db.Post.create(req.body).then(function (dbPost) {
       res.json(dbPost);
+    }).catch(function (err) {
+      console.log(err);
+      res.status(401).send("Add post failed!");
     });
   });
 
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function (req, res) {
+  app.delete("/api/posts/:id", isAuthenticated, function (req, res) {
     db.Post.destroy({
       where: {
         id: req.params.id,
@@ -28,7 +31,6 @@ module.exports = function (app) {
     });
   });
 
-  // PUT route for updating posts
   app.put("/api/posts", isAuthenticated, function (req, res) {
     db.Post.update(req.body, {
       where: {
@@ -36,6 +38,10 @@ module.exports = function (app) {
       },
     }).then(function (dbPost) {
       res.json(dbPost);
+    })
+    .catch(function (err) {
+      console.log(err);
+      res.status(401).send("Edit post failed!");
     });
   });
 };
