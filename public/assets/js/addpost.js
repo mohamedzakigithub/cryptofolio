6a75a6227eca3d13905ec5efafd7619b578c745d
Aliagho <ali.bahrami633@yahoo.com>
2020-05-19 22:21:52 +1000

    fixed git error

--- public/assets/js/addpost.js
+++ public/assets/js/addpost.js
@@ -1,39 +1,46 @@
+const addpostForm = $("form.addpostFrom");
+const titleInput = $("#title-input");
+const postInput = $("#post-input");
+const coinInput = $("#coin-input");
+const priceInput = $("#price-input");
+const typeInput = $("#type-input");
 $(document).ready(function () {
-  var addpostForm = $("form.addpostFrom");
-  var titleInput = $("#title-input");
-  var postInput = $("#post-input");
-
-  addpostForm.on("submit", function (event) {
+  addpostForm.on("submit", async function (event) {
     event.preventDefault();
-    $.get("/api/user_data").then(function (data) {
-      UserId = data.id;
-      event.preventDefault();
-      var postData = {
-        UserId: UserId,
-        title: titleInput.val().trim(),
-        post: postInput.val().trim(),
-      };
+    const result = await $.get("/api/user_data");
+    UserId = result.id;
+    var postData = {
+      UserId: UserId,
+      title: titleInput.val().trim(),
+      post: postInput.val().trim(),
+      coin: coinInput.val().trim(),
+      price: priceInput.val().trim(),
+      type: typeInput.val().trim(),
+    };
 
-      if (!postData.UserId || !postData.title || !postData.post) {
-        $("#alert .msg").text("All fields required");
-        $("#alert").fadeIn(500);
-        return;
-      }
-      addPost(postData);
-    });
+    if (
+      !postData.UserId ||
+      !postData.title ||
+      !postData.post
+    ) {
+      $("#alert .msg").text("All fields required");
+      $("#alert").fadeIn(500);
+      return;
+    }
+    addPost(postData);
   });
 
-  function addPost(postData) {
-    console.log(postData);
-    $.post("/api/posts", postData)
-      .then(function (data) {
-        window.location.replace("/cms");
-      })
-      .catch(handlePostErr);
+  async function addPost(postData) {
+    try {
+      const result = await $.post("/api/posts", postData);
+      window.location.replace("/cms");
+    } catch (error) {
+      errorHandler(error);
+    }
   }
 
-  function handlePostErr(err) {
-    $("#alert .msg").text("Adding post failed");
+  function errorHandler(error){
+    $("#alert .msg").text(error.responseText);
     $("#alert").fadeIn(500);
   }
 });
